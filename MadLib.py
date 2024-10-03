ce218d4b59edd174c54a3ea2e1822f244791953f
Atticus P. Boon <boona@wlhs.wlwv.k12.or.us>
2024-10-03 23:38:04 +0000

    added options

--- MadLib.py
+++ MadLib.py
@@ -1,15 +1,47 @@
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+verb1plural = input("Enter the same verb again but plural: ")
+adjective0 = input("Enter an adjective: ")
+adjective1 = input("Enter an adjective: ")
+verb2 = input("Enter a verb: ")
+verb2plural = input("Enter a verb (Plural): ")
+noun2 = input("Enter a noun: ")
+verb2 = input("Enter a verb: ")
+adjective2 = input("Enter an adjective: ")
+adjectivefood = input("Enter an adjective: ")
+food1 = input("Enter a food: ")
+adjectivefood2 = input("Enter an adjective: ")
+food2 = input("Enter a food: ")
+adjectivefood3 = input("Enter an adjective: ")
+food3 = input("Enter a food: ")
+adjectiveround = input("Enter an adjective: ")
+round1 = input("Enter a round object: ")
+noun3 = input("Enter a noun: ")
+verb3 = input("Enter a verb (Past tense): ")
+verb4 = input("Enter a verb: ")
+noun4 = input("Enter a noun: ")
+verb4plural = input("Enter a vreb (plural): ")
+noun5 = input("Enter a noun: ")
+adjective3 = input("Enter an adjective: ")
+noun5.5 = input("Enter a noun: ")
+noun6 = input("Enter a noun: ")
+noun7 = input("Enter a noun: ")
+verb5 = input("Enter a verb: ")
+noun8 = input("Enter a noun: ")
+adjective4 = input("Enter an adjective: ")
+
 print(f"Once upon a time there was a {noun1} who really liked to {verb1}. /n
       In fact, they liked {verb1plural} so much that they couldn't stop, even when their {adective0} mother called them home for dinner. /n
       Their mother started getting concerned with {noun1}'s addiction to {verb1plural}, that she banned {noun1} from ever {verb1plural} again. /n
-      {noun1} was very sad, until they figured out how to {verb2} the {noun2}. /n 
+      {noun1} was very sad, until they figured out how to {verb2} their {noun2}. /n 
       It made {noun1} feel {adjective1} whenever they {verb2} the {noun2}. /n
-      {noun1} felt so {adjective2} that they killed the {noun2} by {verb2plural} it to death. /n
+      {noun1} felt so {adjective2} that they accidentally killed the {noun2} by {verb2plural} it to death. /n
       In their sadness, they began to eat. /n
       they ate {adjectivefood} {food1} and {adjectivefood2} {food2} and even {adjectivefood3} {food3}. /n
-      {Noun1} ate so much that they were as round as a {adjectiveround} {round}. /n
+      {Noun1} ate so much that they were as round as a {adjectiveround} {round1}. /n
       Since they were so round, they started rolling, slow at first and then faster and faster. /n
       Soon {noun1} crashed into a/an `{noun3}. /n
-      {noun3} was very angry, so he {verb3} {noun1} until they {verb4} like a {noun4}, and went {verb4plural} across the {noun5}. /n
+      {noun3} was very angry, so they {verb3} {noun1} until they {verb4} like a {noun4}, and went {verb4plural} across the {noun5}. /n
       They soon flew through the {adjective3} {noun5.5} of their {noun6} and gave their {noun7} and concussion. /n
       {noun7} had to be {verb5} to the hospital and was given a {noun8} transplant because their old {noun8} was dying. /n
       When {noun7} finally got better, they {verb5} back home and found that {noun1} had eaten all the food in the {noun6}. /n
