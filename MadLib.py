3d5977720f596e54bc7336e7089feb3ebc695bb5
Atticus P. Boon <boona@wlhs.wlwv.k12.or.us>
2024-10-04 00:14:15 +0000

    fixed

--- MadLib.py
+++ MadLib.py
@@ -19,7 +19,7 @@ noun3 = input("Enter a noun: ")
 verb3 = input("Enter a verb (Past tense): ")
 verb4 = input("Enter a verb: ")
 noun4 = input("Enter a noun: ")
-verb4plural = input("Enter a vreb (plural): ")
+verb4plural = input("Enter a verb (plural): ")
 noun5 = input("Enter a noun: ")
 adjective3 = input("Enter an adjective: ")
 noun55 = input("Enter a noun: ")
@@ -29,16 +29,19 @@ verb5 = input("Enter a verb: ")
 noun8 = input("Enter a noun: ")
 adjective4 = input("Enter an adjective: ")
 
-print(f"Once upon a time there was a {noun1} who really liked to {verb1}. \n 
-      In fact, they liked {verb1plural} so much that they couldn't stop, even when their {adjective0} mother called them home for dinner. \n 
-      Their mother started getting concerned with {noun1}'s addiction to {verb1plural}, that she banned {noun1} from ever {verb1plural} again. \n 
-      {noun1} was very sad, until they figured out how to {verb2} their {noun2}. \n 
-      It made {noun1} feel {adjective1} whenever they {verb2} the {noun2}. \n 
-      {noun1} felt so {adjective2} that they accidentally killed the {noun2} by {verb2plural} it to death. /nIn their sadness, they began to eat. \n 
-      They ate {adjectivefood} {food1} and {adjectivefood2} {food2} and even {adjectivefood3} {food3}. /n{Noun1} ate so much that they were as round as a {adjectiveround} {round1}. \n 
-      Since they were so round, they started rolling, slow at first and then faster and faster. /nSoon {noun1} crashed into a/an {noun3}. \n 
-      {noun3} was very angry, so they {verb3} {noun1} until they {verb4} like a {noun4}, and went {verb4plural} across the {noun5}. \n 
-      They soon flew through the {adjective3} {noun55} of their {noun6} and gave their {noun7} and concussion. \n 
-      {noun7} had to be {verb5} to the hospital and was given a {noun8} transplant because their old {noun8} was dying. \n 
-      When {noun7} finally got better, they {verb5} back home and found that {noun1} had eaten all the food in the {noun6}. \n 
-      So {noun7} went and lived at the beach because that was their very {adjective4} dream.")
+print(f"Once upon a time there was a {noun1} who really liked to {verb1}. \n"
+      f"In fact, they liked {verb1plural} so much that they couldn't stop, even when their {adjective0} mother called them home for dinner. \n"
+      f"Their mother started getting concerned with {noun1}'s addiction to {verb1plural}, that she banned {noun1} from ever {verb1plural} again. \n"
+      f"{noun1} was very sad, until they figured out how to {verb2} their {noun2}. \n"
+      f"It made {noun1} feel {adjective1} whenever they {verb2} the {noun2}. \n"
+      f"{noun1} felt so {adjective2} that they accidentally killed the {noun2} by {verb2plural} it to death. \n"
+      f"In their sadness, they began to eat. \n"
+      f"They ate {adjectivefood} {food1} and {adjectivefood2} {food2} and even {adjectivefood3} {food3}. \n"
+      f"{noun1} ate so much that they were as round as a {adjectiveround} {round1}. \n"
+      f"Since they were so round, they started rolling, slow at first and then faster and faster. \n"
+      f"Soon {noun1} crashed into a {noun3}. \n"
+      f"{noun3} was very angry, so they {verb3} {noun1} until they {verb4} like a {noun4}, and went {verb4plural} across the {noun5}. \n"
+      f"They soon flew through the {adjective3} {noun55} of their {noun6} and gave their {noun7} a concussion. \n"
+      f"{noun7} had to be {verb5} to the hospital and was given a {noun8} transplant because their old {noun8} was dying. \n"
+      f"When {noun7} finally got better, they {verb5} back home and found that {noun1} had eaten all the food in the {noun6}. \n"
+      f"So {noun7} went and lived at the beach because that was their very {adjective4} dream.")
