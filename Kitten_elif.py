c754b34af0904da3cc3edb866d78ed6e757c061f
Atticus P. Boon <boona@wlhs.wlwv.k12.or.us>
2024-10-09 15:41:12 +0000

    no dead

--- Kitten_elif.py
+++ Kitten_elif.py
@@ -9,8 +9,7 @@ age = float(input("How old is the cat? (months) "))
 kitten = age <= 6
 teen = 7 < age <= 11
 adult = 12 < age <= 96
-senior = 97 < age < 336
-dead = 337 < age
+senior = 97 < age
 
 print('Approach 1'.center(20,'-'))
 if kitten:
@@ -21,8 +20,6 @@ elif adult:
     print("Price: $125")
 elif senior:
     print("Price: $85")
-elif dead:
-    print("Dead")
 
 time.sleep(1)
 print('Approach 2'.center(20,'-'))
@@ -34,8 +31,6 @@ elif teen:
     print("Price: $200")
 elif kitten:
     print("Price: $250")
-elif dead:
-    print("Dead")
 
 time.sleep(1)
 print('Approach 3'.center(20,'-'))
@@ -47,8 +42,6 @@ if adult:
     print("Price: $125")
 if senior:
     print("Price: $85")
-if dead:
-    print("Dead")
 
 time.sleep(1)
 print('Approach 4'.center(20,'-'))
@@ -60,5 +53,3 @@ if teen:
     print("Price: $200")
 if kitten:
     print("Price: $250")
-if dead:
-    print("Dead")
